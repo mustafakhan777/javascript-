0b773c56016692276ef25529f9bb3fd8e8f803eb
mustafa777 <mustafa.khan2214@gmail.com>
2022-11-16 15:09:53 -0500

    work

---
+++ app.js
@@ -0,0 +1,98 @@
+// alert("Javascript Ale rt")
+// alert("Welcome to Js World")
+// alert("Happy COding")
+// alert("Welcome to js world \n Happy Coding..")
+// console.log("Hello..Ican run JS through my web browser's Console")
+
+// var userName = "Mustafa";
+// var myName = "Mustafa Khan";
+// var p = "Hello World";
+// alert(userName);
+// alert(myName);
+// alert(p);
+// var c = "Pizza \n Pizz \n Piz \n Pi \n P "
+// alert(c);
+// var email =  "example@example.com"
+// alert("My Email address is " + email);
+// var a = "A Smarter \n Way To Learn JS";
+// alert("I am trying to learn from the book " + a)
+// document.write("Yah! I Can Write HTML content through JavaScript");
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(design);
+
+
+// var age = "I am 15 years old"
+// alert(age)
+// var N = "14";
+// alert("You Have visited this site " + N +" times")
+// var birthyear = "1990"
+// document.write("My Birth year is " + birthyear )
+// document.write(" \n Data Type Of my declared variable is number");
+// 
+
+// document.write("A heading stating “Rules for naming JS variables”")
+// document.write("<br>")
+// document.write("Variable names can only contain Numbers, $ and _.For example $my_1stVariable")
+// document.write("<br>")
+// document.write("Variables must begin with a letter, $ or_ .For example $name, _name or name")
+// document.write("<br>")
+// document.write("Variable names are case sensitive")
+// document.write("<br>")
+// document.write("Variable names should not be JS keywords")
+
+// var num1 = 3;
+// var num2 = 5;
+// var add = num1+num2;
+// document.write("Sum o f 3 and 5 is " + add)
+// var num1 = 3;
+// var num2 = 5;
+// var subtract = num1-num2;
+// document.write("Subtraction o f 3 and 5 is " + subtract)
+// var num1 = 3;
+// var num2 = 5;
+// var multiply = num1*num2;
+// document.write("multiplication o f 3 and 5 is " + multiply)
+// var num1 = 3;
+// var num2 = 5;
+// var division = num1/num2;
+// document.write("Division o f 3 and 5 is " + division)
+// var num1 = 3;
+// var num2 = 5;
+// var modulus = num1%num2;
+// document.write("Modulus o f 3 and 5 is " + modulus)
+// var num1;
+// num1 = 5;
+// num1++;
+// document.write("After Icreament is " + num1)
+// num1 = num1+7;
+// document.write("After Icreament is " + num1)
+// num1--;
+// document.write("After decreament is " + num1)
+// num1=num1%3;
+// document.write("After dividing the variable value by 3  is " + num1)
+// var costofoneTicket = 600;
+// var ticketPrice = 600*5;
+// document.write("Total Cost of buy 5 tickets to a movie "+ticketPrice+ " PKR")
+// var num1 = 4;
+// document.write("Table of 4");
+// document.write("<br>")
+// document.write(num1 + " x 1 = " + num1 );
+// document.write("<br>")
+// document.write(num1 + " x 2 = " + num1*2 );
+// document.write("<br>")
+// document.write(num1 + " x 3 = " + num1*3 );
+// document.write("<br>")
+// document.write(num1 + " x 4 = " + num1*4 );
+// document.write("<br>")
+// document.write(num1 + " x 5 = " + num1*5 );
+// document.write("<br>")
+// document.write(num1 + " x 6 = " + num1*6 );
+// document.write("<br>")
+// document.write(num1 + " x 7 = " + num1*7 );
+// document.write("<br>")
+// document.write(num1 + " x 8 = " + num1*8 );
+// document.write("<br>")
+// document.write(num1 + " x 9 = " + num1*9 );
+// document.write("<br>")
+// document.write(num1 + " x 10 = " + num1*10 );
+
